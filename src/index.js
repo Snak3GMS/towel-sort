db41c169fdcbd46cd926ed153f22aa773dee47a0
vova <vova.shkar74@gmail.com>
2021-02-02 14:52:13 +0300

    feat: makes func towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments.length) {
+    for (let i=0; i<matrix.length; i++) {
+    (i % 2 == 1) ? matrix[i].reverse() : 0;
+    }
+  }
+   return (arguments.length && matrix.length ) ? matrix.join(',').split(',').map(Number) : []
 }
